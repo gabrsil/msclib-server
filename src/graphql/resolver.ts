64c027e6724804777150b1d6bdcd981b675242b0
Gabriel <gabriel.aconsurgens@gmail.com>
2022-12-23 01:52:32 -0300

    Update Graphql resolver

--- src/graphql/resolver.ts
+++ src/graphql/resolver.ts
@@ -2,12 +2,14 @@ import album from "../resolvers/album";
 import artist from "../resolvers/artist";
 import auth from "../resolvers/auth";
 import genre from "../resolvers/genre";
+import friendship from "../resolvers/friendship";
 import reproduction from "../resolvers/reproduction";
 
 export default {
   Query: {
     ...album?.query,
     ...artist?.query,
+    ...friendship.query,
     ...reproduction?.query,
   },
   Mutation: {
@@ -16,5 +18,6 @@ export default {
     ...genre?.mutation,
     ...auth?.mutation,
     ...reproduction?.mutation,
+    ...friendship?.mutation,
   },
 };
