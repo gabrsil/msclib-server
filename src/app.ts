7392718f60e2c4592ca2a5ae7e46f9f56ec39e08
Gabriel <gabriel.aconsurgens@gmail.com>
2023-01-07 14:27:55 -0300

    Add graphql constraint directives

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,35 @@
-import { gql, ApolloServer } from 'apollo-server'
-import { TypedQueryDocumentNode } from 'graphql'
-import { resolve } from 'path';
-import resolvers from './graphql/resolver'
-import typeDefs from './graphql/typeDefs';
+import { gql, ApolloServer } from "apollo-server";
+import { makeExecutableSchema } from "graphql-tools";
+import { TypedQueryDocumentNode } from "graphql";
+import {
+  constraintDirective,
+  constraintDirectiveTypeDefs,
+} from "graphql-constraint-directive";
+import resolvers from "./graphql/resolver";
+import typeDefs from "./graphql/typeDefs";
 
-// const requireGQL = (file: string) => 
+// const requireGQL = (file: string) =>
 //   gql`${readFileSync(require.resolve(file)).toString('utf-8')}`
-  // The ApolloServer constructor requires two parameters: your schema
+// The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
+let schema = makeExecutableSchema({
+  typeDefs: [constraintDirectiveTypeDefs, typeDefs],
+});
+
+schema = constraintDirective()(schema);
+
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: ({ res }) => ({ res }),
-    csrfPrevention: true,
-    cors: {
-      credentials: true,
-      // origin: "http://localhost:8080"
-    }
-  });
-  
-  // The `listen` method launches a web server.
-  server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-  });
+  schema,
+  resolvers,
+  context: ({ res }) => ({ res }),
+  csrfPrevention: true,
+  cors: {
+    credentials: true,
+    // origin: "http://localhost:8080"
+  },
+});
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
